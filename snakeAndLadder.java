f1fa819025847eaecc730094f3009c7f949d7100
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-23 13:01:54 +0530

    ADD: Rolled the die to get a number b/w 1 to 6

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class snakeAndLadder{
 
 	public static void main(String[] args){
 
 		int startPoint = 0;
 		int player1 = startPoint;
+		Random roll = new Random();
+		int dice = roll.nextInt(6) + 1;
+		System.out.println(dice);
 	}
 }
