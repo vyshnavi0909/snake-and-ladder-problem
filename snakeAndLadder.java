eb7d9670ef6116e2287f457c824fc5f29848926e
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-23 13:47:21 +0530

    ADD: while loop to reach 100th position

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -8,32 +8,34 @@ public class snakeAndLadder{
 		int startPoint = 0;
 		int player1 = startPoint;
 
-		//rolling the die to get a number b/w 1 and 6
-		Random roll = new Random();
-		int dice = roll.nextInt(6) + 1;
+		while(player1 <= 100){
+			//rolling the die to get a number b/w 1 and 6
+			Random roll = new Random();
+			int dice = roll.nextInt(6) + 1;
 
-		//generating options i.e., 0-noplay 1-ladder 2-snake
-		Random options = new Random();
-		int opt = options.nextInt(3);
+			//generating options i.e., 0-noplay 1-ladder 2-snake
+			Random options = new Random();
+			int opt = options.nextInt(3);
 
-		//checking the options for noPlay or ladder or snake
-		switch(opt){
-			case 0:
-					player1 = player1;
-					System.out.println("Sorry! You can't play and remains at same point");
-					break;
-			case 1:
-					player1 = player1 + dice;
-					System.out.println("Hurray! You got a ladder and you have moved " + player1 + " steps forward");
-					break;
-			case 2:
-					player1 = player1 - dice;
-					System.out.println("Boo! You are bitten by the snake and moved " + player1 + " steps backward");
-					break;
-			default:
-					System.out.println("invaild");
-					break;
+			//checking the options for noPlay or ladder or snake
+			switch(opt){
+				case 0:
+						player1 = player1;
+						break;
+				case 1:
+						player1 = player1 + dice;
+						break;
+				case 2:
+						player1 = player1 - dice;
+						break;
+				default:
+						System.out.println("invaild");
+						break;
+			}
+			if(player1 < 0){
+				player1 = startPoint;
+			}
 		}
-	System.out.println(player1);
+		System.out.println("Hurrah! You have reached 100.");
 	}
 }
